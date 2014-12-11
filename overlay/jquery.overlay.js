64e6d4663ecc10d762fb0626a390a6bb60c679ce
at0717 <git@at0717.com>
2014-12-11 16:31:58 +0800

    fix bug

--- overlay/jquery.overlay.js
+++ overlay/jquery.overlay.js
@@ -11,14 +11,15 @@
     }
     return rv;
 }
-;(function ($) {
+; (function ($) {
+    var id = 'jquery_overlay_place_holder';
     var overlay, holdselect, freeselect, loader, showloader, timer,
         isIE6 = (navigator.userAgent.toLowerCase().indexOf('msie 6') != -1) && getInternetExplorerVersion() < 7 && !window.XMLHttpRequest;
     $.Overlay = function (settings) {
         settings = jQuery.extend({
             canclose: true,
             showloader: false,
-            loaderimg: './img/bar90.gif',
+            loaderimg: '/content/images/bar90.gif',
             overlay_opacity: 0.7,
             canTimeout: false,
             timeout: 10000,
@@ -27,36 +28,42 @@
         }, settings);
 
         this.show = function () {
-            overlay = $('<div style="display:none;"></div>');
-            showloader = settings.showloader;
             if (isIE6) {
                 holdselect = $('<style type="text/css">* html select { visibility: hidden; }</style>');
                 freeselect = $('<style type="text/css">* html select { visibility: visible; }</style>');
             }
-            overlay.css({
-                'position': (isIE6 ? 'absolute' : 'fixed'),
-                'left': 0,
-                'top': 0,
-                'width': $(window).width(),
-                'height': (isIE6 ? $('body').height() : $(window).height()),
-                'background': '#777777',
-                'opacity': settings.overlay_opacity,
-                'z-index': settings.z_index
-            });
 
-            overlay.appendTo('body');
+            showloader = settings.showloader;
 
-            if (showloader) {
-                loader = $('<div><div style="widht:100%;text-align:center;">loading……<div><div style=""><img src="' + settings.loaderimg + '" alt="loading"></div></div>');
-                loader.css({
+            if ($('#' + id).length == 0) {
+                overlay = $('<div id="' + id + '" style="display:none;"></div>');
+
+                overlay.css({
                     'position': (isIE6 ? 'absolute' : 'fixed'),
-                    'left': ($(window).width() - loader.width()) / 2,
-                    'top': ($(window).height() - loader.height()) / 2,
-                    'z-index': settings.z_index + 10
+                    'left': 0,
+                    'top': 0,
+                    'width': $(window).width(),
+                    'height': (isIE6 ? $('body').height() : $(window).height()),
+                    'background': '#777777',
+                    'opacity': settings.overlay_opacity,
+                    'z-index': settings.z_index
                 });
 
-                loader.appendTo('body');
-            }
+                overlay.appendTo('body');
+
+                if (showloader) {
+                    loader = $('<div style="display:none;"><div style="widht:100%;text-align:center;">数据加载中……<div><div style=""><img src="' + settings.loaderimg + '" alt="loading"></div></div>');
+                    loader.css({
+                        'position': (isIE6 ? 'absolute' : 'fixed'),
+                        'left': ($(window).width() - loader.width()) / 2,
+                        'top': ($(window).height() - loader.height()) / 2,
+                        'z-index': settings.z_index + 10
+                    });
+
+                    loader.appendTo('body');
+                }
+            };
+
             overlay.fadeIn('fast', function () {
                 if (settings.showloader) {
                     loader.show();
@@ -76,13 +83,11 @@
 
         this.show();
 
-        
-
         if (settings.canTimeout) {
             timer = Timer(function () {
                 $.Overlay.close();
                 if (null == settings.timeoutcallback) {
-                    alert('timeout!');
+                    alert('操作超时!');
                 } else {
                     settings.timeoutcallback();
                 }
@@ -91,17 +96,21 @@
 
         var t;
     };
+
     $.Overlay.close = function () {
         if (null != timer) {
             timer.pause();
         }
+        
+        overlay.fadeOut('fast', function () {
+            if (showloader) {
+                loader.hide();
+            }
+        });
 
-        if (showloader) {
-            loader.remove();
-        }
-        overlay.fadeOut('fast');
         overlay.unbind('click');
-        overlay.remove();
+
+        overlay.hide();
 
         if (isIE6) {
             holdselect.remove();
